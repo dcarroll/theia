6caaef8f820ef8de49cd296b50ff7b5b913c7c15
Dave Carroll <djcarroll@gmail.com>
2017-11-19 13:27:46 -0800

    dave

--- packages/core/src/node/backend-application.ts
+++ packages/core/src/node/backend-application.ts
@@ -5,40 +5,50 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import * as http from 'http';
-import * as express from 'express';
-import * as yargs from 'yargs';
+import * as http from "http";
+import * as express from "express";
+import * as yargs from "yargs";
 import { inject, named, injectable } from "inversify";
-import { ILogger, ContributionProvider } from '../common';
-import { CliContribution } from './cli';
-import { Deferred } from '../common/promise-util';
-import { BackendProcess } from './backend-process';
+import { ILogger, ContributionProvider } from "../common";
+import { CliContribution } from "./cli";
+import { Deferred } from "../common/promise-util";
+import { BackendProcess } from "./backend-process";
 
-export const BackendApplicationContribution = Symbol("BackendApplicationContribution");
+export const BackendApplicationContribution = Symbol(
+  "BackendApplicationContribution"
+);
 export interface BackendApplicationContribution {
-    initialize?(): void;
-    configure?(app: express.Application): void;
-    onStart?(server: http.Server): void;
+  initialize?(): void;
+  configure?(app: express.Application): void;
+  onStart?(server: http.Server): void;
 }
 
-const defaultPort = BackendProcess.electron ? 0 : 3000;
-const defaultHost = 'localhost';
+const defaultPort = BackendProcess.electron ? 0 : 5000;
+const defaultHost = process.env.THEIAHOST || "localhost";
 
 @injectable()
 export class BackendApplicationCliContribution implements CliContribution {
+  port: number;
+  hostname: string | undefined;
 
-    port: number;
-    hostname: string | undefined;
+  configure(conf: yargs.Argv): void {
+    yargs.option("port", {
+      alias: "p",
+      description: "The port the backend server listens on.",
+      type: "number",
+      default: defaultPort
+    });
+    yargs.option("hostname", {
+      description: "The allowed hostname for connections.",
+      type: "string",
+      default: defaultHost
+    });
+  }
 
-    configure(conf: yargs.Argv): void {
-        yargs.option('port', { alias: 'p', description: 'The port the backend server listens on.', type: 'number', default: defaultPort });
-        yargs.option('hostname', { description: 'The allowed hostname for connections.', type: 'string', default: defaultHost });
-    }
-
-    setArguments(args: yargs.Arguments): void {
-        this.port = args.port;
-        this.hostname = args.hostname;
-    }
+  setArguments(args: yargs.Arguments): void {
+    this.port = args.port;
+    this.hostname = args.hostname;
+  }
 }
 
 /**
@@ -46,72 +56,78 @@ export class BackendApplicationCliContribution implements CliContribution {
  */
 @injectable()
 export class BackendApplication {
+  protected readonly app: express.Application = express();
 
-    protected readonly app: express.Application = express();
-
-    constructor(
-        @inject(ContributionProvider) @named(BackendApplicationContribution)
-        protected readonly contributionsProvider: ContributionProvider<BackendApplicationContribution>,
-        @inject(BackendApplicationCliContribution) protected readonly cliParams: BackendApplicationCliContribution,
-        @inject(ILogger) protected readonly logger: ILogger
-    ) {
-        process.on('uncaughtException', error => {
-            if (error) {
-                logger.error('Uncaught Exception: ', error.toString());
-                if (error.stack) {
-                    logger.error(error.stack);
-                }
-            }
-        });
-
-        for (const contribution of this.contributionsProvider.getContributions()) {
-            if (contribution.initialize) {
-                try {
-                    contribution.initialize();
-                } catch (err) {
-                    this.logger.error(err.toString());
-                }
-            }
+  constructor(
+    @inject(ContributionProvider)
+    @named(BackendApplicationContribution)
+    protected readonly contributionsProvider: ContributionProvider<
+      BackendApplicationContribution
+    >,
+    @inject(BackendApplicationCliContribution)
+    protected readonly cliParams: BackendApplicationCliContribution,
+    @inject(ILogger) protected readonly logger: ILogger
+  ) {
+    process.on("uncaughtException", error => {
+      if (error) {
+        logger.error("Uncaught Exception: ", error.toString());
+        if (error.stack) {
+          logger.error(error.stack);
         }
+      }
+    });
 
-        for (const contribution of this.contributionsProvider.getContributions()) {
-            if (contribution.configure) {
-                try {
-                    contribution.configure(this.app);
-                } catch (err) {
-                    this.logger.error(err.toString());
-                }
-            }
+    for (const contribution of this.contributionsProvider.getContributions()) {
+      if (contribution.initialize) {
+        try {
+          contribution.initialize();
+        } catch (err) {
+          this.logger.error(err.toString());
         }
+      }
     }
 
-    use(...handlers: express.Handler[]): void {
-        this.app.use(...handlers);
-    }
-
-    async start(aPort?: number, aHostname?: string): Promise<http.Server> {
-        const deferred = new Deferred<http.Server>();
-        let server: http.Server;
-        const port = aPort !== undefined ? aPort : this.cliParams.port;
-        const hostname = aHostname !== undefined ? aHostname : this.cliParams.hostname;
-        server = this.app.listen(port, hostname!, () => {
-            this.logger.info(`Theia app listening on http://${hostname || 'localhost'}:${server.address().port}.`);
-            deferred.resolve(server);
-        });
-
-        /* Allow any number of websocket servers.  */
-        server.setMaxListeners(0);
-
-        for (const contrib of this.contributionsProvider.getContributions()) {
-            if (contrib.onStart) {
-                try {
-                    contrib.onStart(server);
-                } catch (err) {
-                    this.logger.error(err.toString());
-                }
-            }
+    for (const contribution of this.contributionsProvider.getContributions()) {
+      if (contribution.configure) {
+        try {
+          contribution.configure(this.app);
+        } catch (err) {
+          this.logger.error(err.toString());
         }
-        return deferred.promise;
+      }
     }
+  }
 
+  use(...handlers: express.Handler[]): void {
+    this.app.use(...handlers);
+  }
+
+  async start(aPort?: number, aHostname?: string): Promise<http.Server> {
+    const deferred = new Deferred<http.Server>();
+    let server: http.Server;
+    const port = aPort !== undefined ? aPort : this.cliParams.port;
+    const hostname =
+      aHostname !== undefined ? aHostname : this.cliParams.hostname;
+    server = this.app.listen(port, hostname!, () => {
+      this.logger.info(
+        `Theia app listening on http://${hostname ||
+          "localhost"}:${server.address().port}.`
+      );
+      deferred.resolve(server);
+    });
+
+    /* Allow any number of websocket servers.  */
+    server.setMaxListeners(0);
+
+    for (const contrib of this.contributionsProvider.getContributions()) {
+      if (contrib.onStart) {
+        try {
+          contrib.onStart(server);
+        } catch (err) {
+          this.logger.error(err.toString());
+        }
+      }
+    }
+    return deferred.promise;
+  }
 }
--- packages/terminal/src/node/terminal-backend-contribution.slow-spec.ts
+++ packages/terminal/src/node/terminal-backend-contribution.slow-spec.ts
@@ -5,14 +5,14 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import * as chai from 'chai';
-import 'mocha';
-import * as chaiAsPromised from 'chai-as-promised';
-import { testContainer } from './test/inversify.spec-config';
-import { BackendApplication } from '@theia/core/lib/node/backend-application';
-import { IShellTerminalServer } from '../common/shell-terminal-protocol';
-import * as ws from 'ws';
-import * as http from 'http';
+import * as chai from "chai";
+import "mocha";
+import * as chaiAsPromised from "chai-as-promised";
+import { testContainer } from "./test/inversify.spec-config";
+import { BackendApplication } from "@theia/core/lib/node/backend-application";
+import { IShellTerminalServer } from "../common/shell-terminal-protocol";
+import * as ws from "ws";
+import * as http from "http";
 
 chai.use(chaiAsPromised);
 
@@ -22,30 +22,32 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
-describe('Terminal Backend Contribution', function () {
+describe("Terminal Backend Contribution", function() {
+  this.timeout(10000);
+  let server: http.Server;
+  let shellTerminalServer: IShellTerminalServer;
 
-    this.timeout(10000);
-    let server: http.Server;
-    let shellTerminalServer: IShellTerminalServer;
+  before(async function() {
+    const application = testContainer.get(BackendApplication);
+    shellTerminalServer = testContainer.get(IShellTerminalServer);
+    server = await application.start();
+  });
 
-    before(async function () {
-        const application = testContainer.get(BackendApplication);
-        shellTerminalServer = testContainer.get(IShellTerminalServer);
-        server = await application.start();
+  it("is data received from the terminal ws server", async function() {
+    const terminalId = await shellTerminalServer.create({});
+    const p = new Promise((resolve, reject) => {
+      const socket = new ws(
+        `ws://localhost:${server.address()
+          .port}/services/terminals/${terminalId}`
+      );
+      socket.on("message", msg => {
+        resolve();
+        socket.close();
+      });
+      socket.on("error", error => {
+        reject(error);
+      });
     });
-
-    it("is data received from the terminal ws server", async function () {
-        const terminalId = await shellTerminalServer.create({});
-        const p = new Promise((resolve, reject) => {
-            const socket = new ws(`ws://localhost:${server.address().port}/services/terminals/${terminalId}`);
-            socket.on('message', msg => {
-                resolve();
-                socket.close();
-            });
-            socket.on('error', error => {
-                reject(error);
-            });
-        });
-        return expect(p).to.be.eventually.fulfilled;
-    });
+    return expect(p).to.be.eventually.fulfilled;
+  });
 });
